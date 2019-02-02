86c214219a7f15635dd79fdd22292f6fb7adf91a
jolaleye <j.olaleye.o@gmail.com>
2019-02-02 13:59:58 -0600

    Configure file uploads

--- api/index.js
+++ api/index.js
@@ -1,5 +1,37 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-body');
+const path = require('path');
+const fs = require('fs-extra');
+const shortid = require('shortid');
 
 const app = new Koa();
+const router = new Router();
+
+// ensure the upload directory exists
+const uploadDir = path.join(__dirname, 'uploads');
+fs.ensureDirSync(uploadDir);
+
+// configure file uploads
+app.use(
+  bodyParser({
+    multipart: true,
+    formidable: {
+      keepExtensions: true,
+      uploadDir,
+      onFileBegin: (name, file) => {
+        // rename the file with a short id
+        file.path = path.join(uploadDir, `${shortid.generate()}${path.extname(file.name)}`);
+      }
+    }
+  })
+);
+
+router.post('/upload', async (ctx, next) => {
+  const { file } = ctx.request.files;
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
