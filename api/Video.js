fa0aa27fd2bd7ded593d5cf6fb77d17b7ce49dc9
jolaleye <j.olaleye.o@gmail.com>
2019-02-03 16:32:50 -0600

    Process video with ffmpeg

--- api/Video.js
+++ api/Video.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const shortid = require('shortid');
+const { exec } = require('child_process');
+const { promisify } = require('util');
 
 const _ = require('./config.json');
 
@@ -10,7 +13,11 @@ class Video {
 
   async process() {
     const outputFormat = this.options.format || path.extname(this.file.name).slice(1);
-    return this.file.path;
+    const outputPath = path.join(path.dirname(this.file.path), `${shortid.generate()}.${outputFormat}`);
+
+    await promisify(exec)(`ffmpeg -i ${this.file.path} ${outputPath}`);
+
+    return outputPath;
   }
 
   validate() {
