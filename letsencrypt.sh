a5f4a7fb6ebb514f170ec18aaa87e089862512ac
jolaleye <j.olaleye.o@gmail.com>
2019-02-19 22:49:02 -0600

    Add new letsencrypt script

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -4,8 +4,9 @@ domains=(api.altr.app)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="j.olaleye.o@gmail.com"
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0
 
+# check for already existing certbot data
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
@@ -13,7 +14,7 @@ if [ -d "$data_path" ]; then
   fi
 fi
 
-
+## download nginx tls directives from let's encrypt
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
@@ -22,6 +23,7 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
+# make dummy certificates
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
@@ -32,11 +34,12 @@ docker-compose run --rm --entrypoint "\
     -subj '/CN=localhost'" certbot
 echo
 
-
+# start nginx container
 echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
 echo
 
+# remove the dummy certificates
 echo "### Deleting dummy certificate for $domains ..."
 docker-compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
@@ -46,21 +49,19 @@ echo
 
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
 domain_args=""
 for domain in "${domains[@]}"; do
   domain_args="$domain_args -d $domain"
 done
 
-# Select appropriate email arg
 case "$email" in
   "") email_arg="--register-unsafely-without-email" ;;
   *) email_arg="--email $email" ;;
 esac
 
-# Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
+# request real certificates
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
@@ -71,5 +72,6 @@ docker-compose run --rm --entrypoint "\
     --force-renewal" certbot
 echo
 
-echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+# stop nginx container
+echo "### Stopping nginx ..."
+docker-compose rm -s -f nginx
